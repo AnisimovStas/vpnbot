4e90a2ed3f235e512207e98aefd3df8da76256dd
Stanislav <AnisimovSA@apheco.ru>
2024-07-15 14:02:25 +0400

    feat: init commit

---
+++ src/main/java/com/feral/vpnbot/VpnbotApplication.java
@@ -0,0 +1,13 @@
+package com.feral.vpnbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpnbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feral/vpnbot/VpnbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feral.vpnbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpnbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
